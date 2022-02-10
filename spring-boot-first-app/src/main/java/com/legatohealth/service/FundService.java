0e758acd24e8240e34618b3d22d3f0f001ab6f2b
sivatejaboyapati <boyapatisivateja@gmail.com>
2022-02-10 14:38:42 +0530

    update

---
+++ spring-boot-first-app/src/main/java/com/legatohealth/service/FundService.java
@@ -0,0 +1,18 @@
+package com.legatohealth.service;
+
+import java.math.BigInteger;
+
+import com.legatohealth.beans.FundEntity;
+
+public interface FundService {
+	
+	//public FundEntity funds(int id);
+	//public accountnunum AccountNumberFinfById(int id);
+	public FundEntity showFundsByAcctNum(BigInteger accountnumber);
+	public void addFunds(FundEntity fundentity);
+	public void deductFunds(FundEntity fundentity);
+	
+
+	
+
+}
