5689912789073477b6079e46f05ef11c35597390
ravindra <ravindrareddy.velturla@gmail.com>
2022-02-12 00:33:43 +0530

    springboot projeect after mapping table product and employee

--- spring-boot-first-app/src/main/java/com/legatohealth/beans/Cart.java
+++ spring-boot-first-app/src/main/java/com/legatohealth/beans/Cart.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,9 +15,6 @@ public class Cart {
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
-	@ManyToOne
-	private ProductEntity productentity;
 
 	public Cart(int id) {
 		super();
--- spring-boot-first-app/src/main/java/com/legatohealth/service/CartServiceImpl.java
+++ spring-boot-first-app/src/main/java/com/legatohealth/service/CartServiceImpl.java
@@ -1,68 +1,35 @@
 package com.legatohealth.service;
 
-<<<<<<< HEAD
-=======
-import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.ManyToOne;
-
->>>>>>> master
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-<<<<<<< HEAD
 import com.legatohealth.dao.EmployeeDao;
 
-=======
-
-import com.legatohealth.beans.Cart;
-import com.legatohealth.beans.ProductEntity;
-import com.legatohealth.beans.UserEntity;
-import com.legatohealth.dao.CartDao;
-import com.legatohealth.dao.EmployeeDao;
-import com.legatohealth.dao.UserDao;
-import com.legatohealth.dao.UserEntityDao;
-import com.legatohealth.exceptions.CartNotFoundException;
-import com.legatohealth.exceptions.ProductNotFound;
-import com.legatohealth.exceptions.UserNotFoundException;
->>>>>>> master
 
 @Service
 public class CartServiceImpl implements CartService {
 	@Autowired
-	private CartDao dao;
+	private EmployeeDao dao;// injects the proxy object of EmployeeDao
 	@Autowired
-	@ManyToOne
-	private ProductEntity productentity;
+	private ProductService productservice;
 	@Override
 	@Transactional 
-	public Cart selectItems(int id) {
-		productentity.getProductname();
+	public CartService selectItems(CartService cartservice) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
-	public Cart deleteItems(int id) throws CartNotFoundException {
-		Cart cart=(Cart) viewItems(id);
-		dao.delete(cart);
-		
+	public CartService deleteItems(CartService cartservice) {
+		// TODO Auto-generated method stub
 		return null;
-		
 	}
 	@Override
-	public Cart viewItems(int id) throws CartNotFoundException{
-		Cart cart=null;
-		Optional<Cart> optional = dao.findById(id);
-		if (optional.isPresent()) {
-			cart = optional.get();
-		} else {
-			throw new CartNotFoundException("Cart with ID : " + id + "is not present");
-		}
-		
-		return (Cart) cart;
+	public CartService viewItems(CartService cartservice) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	@Override
-	public Cart checkout(CartService cartservice) {
+	public CartService checkout(CartService cartservice) {
 		// TODO Auto-generated method stub
 		return null;
 	}
