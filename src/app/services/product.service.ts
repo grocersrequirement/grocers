cf021f67ec54bb89665ed8347a2c6a45466d8a1c
SANKETJAIN1111 <sigmasanket2712@gmail.com>
2022-02-12 13:40:16 +0530

    Add product :

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,29 +7,29 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
   constructor(private _client: HttpClient) { }
-  baseUrl = 'http://localhost:9090'
+  baseUrl = 'http://localhost:9090/product'
 
   storeData(userData : any):Observable<any>{
-    let url =`${this.baseUrl}/product`;
+    let url =`${this.baseUrl}`;
     
     return this._client.post(url,userData);
   }
   updateData(userId:number, name :string ,salary : number):Observable<any>{
-    let url =`${this.baseUrl}/user/${userId}/${name}/${salary}`;
+    let url =`${this.baseUrl}/updateproduct/${userId}/${name}/${salary}`;
     return this._client.put(url,undefined);
   }
 
   fetchDatas():Observable<any>{
-    let url =`${this.baseUrl}/user`;
+    let url =`${this.baseUrl}/fetchproducts`;
     console.log(url);
     return this._client.get(url);
   }
   fetchData(userId : number):Observable<any>{
-    let url =`${this.baseUrl}/user/${userId}`;
+    let url =`${this.baseUrl}/fetchproduct/${userId}`;
     return this._client.get(url);
   }
   deleteData(userId : number):Observable<any>{
-    let url =`${this.baseUrl}/user/${userId}`;
+    let url =`${this.baseUrl}/deleteproduct/${userId}`;
     return this._client.delete(url);
   }
  
