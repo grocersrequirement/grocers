338a8f106a685fd7d960e2a43cf830d243fe6ba9
VineethPasunuri <pasunurivineeth11@gmail.com>
2022-02-16 19:37:17 +0530

    last update product order and cart

--- spring-boot-first-app/src/main/java/com/legatohealth/controller/OrdersController.java
+++ spring-boot-first-app/src/main/java/com/legatohealth/controller/OrdersController.java
@@ -14,8 +14,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.legatohealth.beans.CustomMessage;
 import com.legatohealth.beans.Orders;
 import com.legatohealth.beans.ProductEntity;
+import com.legatohealth.exceptions.OrdersNotFound;
+import com.legatohealth.exceptions.ProductNotFound;
 import com.legatohealth.service.OrdersService;
 @CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
@@ -25,20 +28,40 @@ public class OrdersController {
 	@Autowired
 	private OrdersService orderservice;
 	
-	@GetMapping(path = "/showproducts/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> fetchProducts(@PathVariable("id")int cid){
+	@GetMapping(path = "/showproduct", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> fetchProducts(){
 		ResponseEntity<Object> response = null;
-		Set<ProductEntity> list = orderservice.showproducts(cid);
+		Set<ProductEntity> list = orderservice.showproducts();
 		response = ResponseEntity.status(HttpStatus.OK).body(list);
 		return response;
 	}
 	
-	@PutMapping(path = "/updatestatus/{status}", consumes=MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> setstatus(String status){
+	@PutMapping(path = "/updatestatus/{oid}/{status}", consumes=MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> setstatus(@PathVariable(value="oid")int oid, @PathVariable(value="status")String status){
 		ResponseEntity<Object> response = null;
-		Orders order=orderservice.updatestatus(status);
+		Orders order = null;
+		try {
+			order = orderservice.updatestatus(oid,status);
+		} catch (OrdersNotFound e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		response = ResponseEntity.status(HttpStatus.OK).body(order);
 		return response;
 	}
 	
+	
+	@GetMapping(path = "/getOrder/{oid}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> fetchOrder(@PathVariable(value="oid") int oid) {
+		ResponseEntity<Object> response = null;
+		try {
+			Orders order=orderservice.fetchOrder(oid);
+			//CustomMessage custom = new CustomMessage("User with an id "+id+" deleted", 200);
+			response = ResponseEntity.status(HttpStatus.OK).body(order);
+		} catch (OrdersNotFound e) {
+			CustomMessage custom = new CustomMessage(e.getMessage(), 404);
+			response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(custom);
+		}
+		return response;
+	}
 }
